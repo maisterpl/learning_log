ba4a6ed9a6e9896017fdc461b41f6526efb19fbc
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-01-25 11:45:47 +0100

    first commit learningl_log web app

---
+++ learning_logs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # main site
+    path('', views.index, name='index'),
+    # show all topics
+    path('topics/', views.topics, name='topics'),
+]
